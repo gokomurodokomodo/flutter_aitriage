528cc452de576bc7cbe9149b703882dd391b923e
DESKTOP-LPVLNCB\DELL <hieunp1901@gmail.com>
2023-08-02 14:55:54 +0700

    init

---
+++ lib/aitriage_core/common/app_error.dart
@@ -0,0 +1,15 @@
+enum AppErrorType { networkError, cachedError }
+
+class AppError {
+  final AppErrorType errorType;
+  final int statusCode;
+  final String message;
+  final dynamic body;
+
+  AppError({
+    required this.errorType,
+    this.statusCode = 0,
+    this.message = '',
+    this.body
+  });
+}
