02dcf99b87ef1661197b6a0f74a9dc0c6b9c056b
DESKTOP-LPVLNCB\DELL <hieunp1901@gmail.com>
2023-08-04 14:20:45 +0700

    add main screen

--- lib/aitriage_module_main/feature/main_screen/main_screen.dart
+++ lib/aitriage_module_main/feature/main_screen/main_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aitriage/aitriage_core/ui/widget/device_detector.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return const DeviceDetector(
+        tablet: MainScreenTablet(),
+        phone: MainScreenPhone()
+    );
+  }
+}
+
+class MainScreenTablet extends StatelessWidget {
+  const MainScreenTablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
+
+class MainScreenPhone extends StatelessWidget {
+  const MainScreenPhone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(),
+    );
+  }
+}
+
+
