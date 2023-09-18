242c743d2d56b764dbfe46fda151b2d5e99c949a
DESKTOP-LPVLNCB\DELL <hieunp1901@gmail.com>
2023-09-18 16:17:37 +0700

    fake country id = 240

--- lib/aitriage_module_main/feature/home_main/home_main_controller.dart
+++ lib/aitriage_module_main/feature/home_main/home_main_controller.dart
@@ -31,7 +31,7 @@ class HomeMainController extends GetxController {
   void _getListLocation() async {
     try {
       final resp = await _getListLocationUC.execute('2');
-      _location = resp.data.first;
+      _location = resp.data.where((element) => element.countryId == 240).first;
     } catch (e) {
 
     }
